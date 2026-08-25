712f5facf2b434201a25b8c94b2c26da03f42351
bhumikam925 <bhumikam835@gmail.com>
2026-08-25 18:06:24 +0530

    day32

--- utils.js
+++ utils.js
@@ -1,1 +1,17 @@
+export function debounce(func, delay) {
 
+    let timeout;
+
+    return function (...args) {
+
+        clearTimeout(timeout);
+
+        timeout = setTimeout(() => {
+
+            func.apply(this, args);
+
+        }, delay);
+
+    };
+
+}
