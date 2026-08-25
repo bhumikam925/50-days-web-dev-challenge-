69c93d4ba310d42f7d14ebd043cba9d39181516e
bhumikam925 <bhumikam835@gmail.com>
2026-08-26 01:03:13 +0530

    day34

--- utils.js
+++ utils.js
@@ -15,3 +15,38 @@ export function debounce(func, delay) {
     };
 
 }
+export async function fetchWithRetry(
+    url,
+    options = {},
+    retries = 3,
+    backoff = 500
+) {
+
+    for (let i = 0; i < retries; i++) {
+
+        try {
+
+            const response = await fetch(url, options);
+
+            if (response.ok) {
+                return response;
+            }
+
+            throw new Error(
+                `Request failed with status ${response.status}`
+            );
+
+        } catch (error) {
+
+            if (i === retries - 1) {
+                throw error;
+            }
+
+            await new Promise(
+                resolve => setTimeout(resolve, backoff)
+            );
+
+            backoff *= 2;
+        }
+    }
+}
