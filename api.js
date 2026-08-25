64ab5077208f278907844a1652509658d8a3da14
bhumikam925 <bhumikam835@gmail.com>
2026-08-25 18:23:06 +0530

    day32

--- api.js
+++ api.js
@@ -1,1 +1,33 @@
+export async function fetchRepositories(
+    username
+) {
+
+    const reposGrid =
+        document.getElementById(
+            "repos-grid"
+        );
+
+
+    if (!reposGrid) return;
+
+
+    reposGrid.innerHTML =
+        "<p>Loading repositories...</p>";
+
+
+    try {
+
+        const response =
+            await fetch(
+                `https://api.github.com/users/${username}/repos?sort=updated&per_page=6`
+            );
+
+
+        if (!response.ok) {
+
+            throw new Error(
+                "Unable to fetch repositories."
+            );
+
+        }
 
