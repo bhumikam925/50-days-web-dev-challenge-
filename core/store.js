160fb6034a33e9d63e2607930f684e8238774fff
bhumikam925 <bhumikam835@gmail.com>
2026-09-06 23:01:56 +0530

    day43

---
+++ core/store.js
@@ -0,0 +1,21 @@
+class StateStore {
+    constructor(initialState) {
+        this.state = initialState;
+        this.listeners = [];
+    }
+
+    subscribe(listenerFunction) {
+        this.listeners.push(listenerFunction);
+    }
+
+    setState(newState) {
+        this.state = { ...this.state, ...newState };
+
+        this.listeners.forEach(listener => listener(this.state));
+    }
+}
+
+export const globalStore = new StateStore({
+    cartCount: 0,
+    userTheme: 'light'
+});
